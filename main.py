cf1d9d9e1af1d5cdde44cfad0b02b8ae873ce1f1
coops-pdtpro <cooper-da@hotmail.com>
2024-11-10 16:08:18 +0000

    Day 25: Add guide lines to all pages

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ for index, row in df.iterrows():
     # Set the header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align='L',
-             ln=1)
-    pdf.line(10, 20, 200, 20)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align='L', ln=1)
+
+    # Draw lines down the page
+    for y in range(30, 297, 10):  # Start at 30mm, end at 297mm (A4 height), step 10mm
+        pdf.line(10, y, 200, y)
 
     # Set the footer
     pdf.ln(265)
@@ -25,10 +27,14 @@ for index, row in df.iterrows():
     for i in range(row["Pages"] - 1):
         pdf.add_page()
 
+        # Draw lines down the page
+        for y in range(10, 297, 10):  # Start at 10mm, end at 297mm (A4 height), step 10mm
+            pdf.line(10, y, 200, y)
+
         # Set the footer
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
-pdf.output("output.pdf")
+pdf.output("output_lined.pdf")
